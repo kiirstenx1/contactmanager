b55844f1d0902c68d58e1230639e726ba8508c9b
BethMwangi <wanjikumwangib@gmail.com>
2018-10-15 17:44:24 +0300

    add state management and props

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,27 +2,45 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 export default class Contact extends Component {
-  state = {};
-  onShowClick = () => {
-    console.log(this.state);
+  state = {
+    showContactInfo: true
+  };
+  onShowClick = e => {
+    this.setState({ showContactInfo: !this.state.showContactInfo });
+  };
+  onDeleteClick = () => {
+    this.props.deleteClickHandler();
   };
   render() {
     const { name, email, phone } = this.props.contact;
+    const { showContactInfo } = this.state;
     return (
       <div className="card card-body mb-3">
         <h4>
           {name}
-          <i onClick={this.onShowClick} className="fas fa-sort-down" />
+          <i
+            onClick={this.onShowClick}
+            className="fas fa-sort-down"
+            style={{ cursor: "pointer" }}
+          />
+          <i
+            onClick={this.onDeleteClick}
+            className="fas fa-times"
+            style={{ cursor: "pointer", float: "right", color: "red" }}
+          />
         </h4>
-        <ul className="list-group">
-          <li className="list-group-item">Email: {email}</li>
-          <li className="list-group-item">Phone: {phone}</li>
-        </ul>
+        {showContactInfo ? (
+          <ul className="list-group">
+            <li className="list-group-item">Email: {email}</li>
+            <li className="list-group-item">Phone: {phone}</li>
+          </ul>
+        ) : null}
       </div>
     );
   }
 }
 
 Contact.propTypes = {
-  contact: PropTypes.object.isRequired
+  contact: PropTypes.object.isRequired,
+  deleteClickHandler: PropTypes.func.isRequired
 };
