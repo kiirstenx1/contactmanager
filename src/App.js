cdd3d45d949244104fcb5c8890e37053a8921f2f
BethMwangi <wanjikumwangib@gmail.com>
2018-10-18 17:31:12 +0300

    create add contact form

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import React, { Component } from "react";
-import Contacts from "./components/Contacts";
-import Header from "./components/Header";
+import Contacts from "./components/contacts/Contacts";
+import AddContact from "./components/contacts/AddContact";
+import Header from "./components/layout/Header";
+import { Provider } from "./context";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    name: '',
+  email:'',
+phone:''  }
   render() {
     return (
-      <div className="App">
-        <Header branding="Contact Manager" />
-        <div className="container">
-          <Contacts />
+      <Provider>
+        <div className="App">
+          <Header branding="Contact Manager" />
+          <div className="container">
+          <AddContact />
+            <Contacts />
+          </div>
         </div>
-      </div>
+      </Provider>
     );
   }
 }
